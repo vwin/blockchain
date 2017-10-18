51ee6c6ab37314a9eabf7c3a060dfe1907b4f8b6
Caesar.v <vwin@users.noreply.github.com>
2017-10-18 14:05:26 +0800

    fix running problem

--- blockchain.py
+++ blockchain.py
@@ -132,7 +132,7 @@ class Blockchain:
         return self.last_block['index'] + 1
 
     @property
-    def last_block(self) -> Dict[str: Any]:
+    def last_block(self) -> Dict[str, Any]:
         return self.chain[-1]
 
     @staticmethod
@@ -282,4 +282,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
